61145957c3193f3972d9249b5771f733dd58efd6
Zh2480958488 <50812983+Zh2480958488@users.noreply.github.com>
2019-05-30 19:43:16 +0800

    字符串

---
+++ JS李立超/07字符串/JS.js
@@ -0,0 +1,26 @@
+/*数据类型
+基本数据类型
+	String  	字符串
+	Number		数值
+	Boolean		布尔
+	Null		空值
+	Undefined	未定义
+引用数据类型
+	Object		对象
+
+*/
+
+var str = "Hello Word";//使用双(单)引号不可嵌套使用
+/*转义字符  \
+	\"  \"  双引号
+	\n  	换行
+	\t 		制表符
+	\\		斜杠
+
+
+*/
+
+
+
+
+
