94ab75b6c84f65b975b805c2eb7730efd92c54bb
Zh2480958488 <50812983+Zh2480958488@users.noreply.github.com>
2019-06-02 16:48:39 +0800

    运算符

--- JS李立超/14转换为boolean/JS.js
+++ JS李立超/14转换为boolean/JS.js
@@ -1,7 +1,13 @@
+/*将其他数值类型转换为Boolean类型
+	- 使用Boolean()函数
 
+	只有 0 null undefined false NaN ""转换为false
+*/
 
-
-
+var demo = "a";
+demo = Boolean(demo);
+console.log(typeof(demo));
+console.log(demo);
 
 
 
